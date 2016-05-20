c2355d0d2e4455fc2b8a48923edca481a7ce4edb
Anton Goncharuk <antonge92@gmail.com>
2016-05-20 15:08:06 +0300

    Create migration for comments (table). Update schema and routes. Setup root route. Delete jquery & jquery_ujs from app js. Setup relations between products and comments.

---
+++ db/migrate/20160520113852_create_comments.rb
@@ -0,0 +1,15 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :author, default: 'Anonymous', null: false
+      t.text :body, null: false
+      t.string :ip
+      t.references :product, foreign_key: true
+      t.boolean :answer, defaut: false, null: false
+      t.boolean :approved, default: false, null: false
+      t.integer :rank, default: 0, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519210900) do
+ActiveRecord::Schema.define(version: 20160520113852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author",     default: "Anonymous", null: false
+    t.text     "body",                             null: false
+    t.string   "ip"
+    t.integer  "product_id"
+    t.boolean  "answer",                           null: false
+    t.boolean  "approved",   default: false,       null: false
+    t.integer  "rank",       default: 0,           null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["product_id"], name: "index_comments_on_product_id", using: :btree
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "sku",                limit: 25
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 20160519210900) do
     t.datetime "updated_at",                                                            null: false
   end
 
+  add_foreign_key "comments", "products"
 end
