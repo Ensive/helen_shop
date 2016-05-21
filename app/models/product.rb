55c3064b317664ee3fe5ac600aec7b16ab6434af
Anton Goncharuk <antonge92@gmail.com>
2016-05-21 17:59:15 +0300

    Style header. Add sass variables. Update application css, use border-box. Link fonts in app.html. Organize importing in app.css. Apply font-size in rem. Add ProductsController.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
+
+  def to_s
+    name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   resources :comments
 
-  root 'products#index'
+  # root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
