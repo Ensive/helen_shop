988f3a25f56e8745ff27a2b8d6ba670261a37015
Anton Goncharuk <antonge92@gmail.com>
2016-05-21 12:27:52 +0300

    Provide some seed data for products table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+products_list = [
+    # sku, name, category_id, image_url, description, weight, in_stock, available, discount, discount_available, price
+    # @todo: consider to move sku, image_url, in_stock columns to the separate tables
+    [nil, 'Костюм "JEEP" зеленый', nil, 'jeep-green.jpg', 'Хороший костюм, недорого.', 250, 2, true, 0, false, 170],
+    [nil, 'Платье "С завязками"', nil, 'dress-with-ties.jpg', 'Шикарное платье. Довольно оригинально выглядит.', 400, 1, true, 0, false, 170]
+]
+
+products_list.each do |sku, name, category_id, image_url, description, weight, in_stock, available, discount, discount_available, price|
+  Product.create!(
+      sku: sku,
+      name: name,
+      category_id: category_id,
+      image_url: image_url,
+      description: description,
+      weight: weight,
+      in_stock: in_stock,
+      available: available,
+      discount: discount,
+      discount_available: discount_available,
+      price: price
+  )
+end
