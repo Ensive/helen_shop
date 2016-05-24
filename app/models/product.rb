de05f9566befaee56e58867d7571d41bff780d54
Anton Goncharuk <antonge92@gmail.com>
2016-05-24 23:39:26 +0300

    Uncomment validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
-  # validates :sku, :name, :category_id, :description, presence: true
-  # validates :price, numericality:  {greater_than_or_equal_to: 0.01}
-  # validates :name, uniqueness: true
+  validates :sku, :name, :category_id, :description, presence: true
+  validates :price, numericality:  {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'URL should reference to the image'
