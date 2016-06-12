6f018a61554b8726b46be5d7644064d831f90eb8
Anton Goncharuk <antonge92@gmail.com>
2016-06-12 20:17:03 +0300

    Delete commented mock data.

--- client/assets/javascripts/stores/CommentStore.js
+++ client/assets/javascripts/stores/CommentStore.js
@@ -7,29 +7,7 @@ class CommentEventEmitter extends EventEmitter {
 
   constructor() {
     super();
-    this._comments = [
-      // {
-      //   id: 1,
-      //   author: 'Kathryn',
-      //   body: `Dress is perfect to a birthday girl. It's beautiful with brilliant colours. I just see the pictures from my baby party and she looks adorable on that.`,
-      //   rank: 5,
-      //   created_at: '2 hours'
-      // },
-      // {
-      //   id: 2,
-      //   author: 'Daniel',
-      //   body: `Very friendly customer service and really affordable price. Good choice to choose custom size! Just received my beautiful dress. Wow, really a surprise. Fit my body shape so well! I will be back for sure! Thank you!`,
-      //   rank: 3,
-      //   created_at: 'Yesterday'
-      // },
-      // {
-      //   id: 3,
-      //   author: 'Samantha',
-      //   body: `I bought a dress here for my sister. She loves it very much. She told me it arrived so fast and the quality is good. She asked for your website and want to buy another dress. Thanks!`,
-      //   rank: 4,
-      //   created_at: 'Yesterday'
-      // }
-    ];
+    this._comments = [];
   }
 
   addComment(comment) {
