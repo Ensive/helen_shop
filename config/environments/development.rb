d0bf37b02285f958a5a67a590e18fb852028ee7b
Anton Goncharuk <antonge92@gmail.com>
2016-07-03 15:12:31 +0300

    Switch compression off.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # compressor
   # config.assets.js_compressor = :uglifier
-  config.assets.js_compressor = Uglifier.new(copyright: false)
+  # config.assets.js_compressor = Uglifier.new(copyright: false)
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
