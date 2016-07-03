3be93e05cef8f500e82f5870b267817c18f52c5f
Anton Goncharuk <antonge92@gmail.com>
2016-07-04 00:43:41 +0300

    Test to_s method for Product. Add more configuration to rails_helper.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Product do
 
+  it 'returns name when to_s is called' do
+    p = Product.new(sku: '12345', name: 'New Product', category_id: 1, description: 'Product desc')
+    expect("#{p}").to eq 'New Product'
+  end
 end
