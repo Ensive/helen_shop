758028b3db4aec90b9a15a0b534bc7910bf4681f
Anton Goncharuk <antonge92@gmail.com>
2017-03-03 01:44:09 +0200

    Clean up and update main.js.jsx file

--- client/assets/javascripts/main.js.jsx
+++ client/assets/javascripts/main.js.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import CommentSection from './components/CommentSection.react'
+import { render } from 'react-dom';
 
 function reactRender(id, component, props) {
   let reactNode = document.getElementById(id);
 
   if (reactNode) {
-    // ReactDOM.render(<CommentSection />, reactNode);
-    var componentToRender = require(`./components/${component}.react`);
-    ReactDOM.render(React.createElement(componentToRender, props), reactNode);
+    const componentToRender = require(`./components/${component}.react`);
+    render(React.createElement(componentToRender, props), reactNode);
   }
 }
 
 window.reactRender = reactRender;
-
-// function ready(fn) {
-//   if (document.readyState != 'loading') {
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-
-// ready(reactRender);
